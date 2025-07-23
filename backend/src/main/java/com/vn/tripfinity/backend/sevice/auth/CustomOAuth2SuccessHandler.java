860dcd3a87ec93342b55b5849d00d2fc158bf277
nguyencong293 <nguyenthanhcong210903@gmail.com>
2025-07-23 18:21:24 +0700

    cre token gg

--- backend/src/main/java/com/vn/tripfinity/backend/sevice/auth/CustomOAuth2SuccessHandler.java
+++ backend/src/main/java/com/vn/tripfinity/backend/sevice/auth/CustomOAuth2SuccessHandler.java
@@ -4,17 +4,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vn.tripfinity.backend.model.User;
 import com.vn.tripfinity.backend.repository.UserRepository;
 import com.vn.tripfinity.backend.sevice.UserService;
+import com.vn.tripfinity.backend.sevice.auth.token.JwtTokenProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -27,6 +31,8 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
     private PasswordEncoder passwordEncoder;
     @Autowired
     private UserService userService;
+    @Autowired
+    private JwtTokenProvider tokenProvider;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
@@ -39,22 +45,30 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
         User user = userRepository.findByEmail(email).orElse(null);
         if (user == null) {
             String randomPassword = UUID.randomUUID().toString();
-            User newUser = new User();
-            newUser.setEmail(email);
-            newUser.setFullName(name);
-            newUser.setAvatarUrl(avatar);
-            newUser.setAccountRole(User.AccountRole.tourist);
-            newUser.setAccountStatus(User.AccountStatus.active);
-            newUser.setPasswordHash(passwordEncoder.encode(randomPassword));
-            userRepository.save(newUser);
-
-            userService.sendWelcomeEmail(newUser);
+            user = new User();
+            user.setEmail(email);
+            user.setFullName(name);
+            user.setAvatarUrl(avatar);
+            user.setAccountRole(User.AccountRole.tourist);
+            user.setAccountStatus(User.AccountStatus.active);
+            user.setPasswordHash(passwordEncoder.encode(randomPassword));
+            userRepository.save(user);
+            userService.sendWelcomeEmail(user);
         }
-        // Trả về JSON message thông báo đăng nhập thành công
+        UserDetails userDetails = new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPasswordHash(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getAccountRole().name()))
+        );
+        // Sinh token JWT
+        String jwt = tokenProvider.generateToken(userDetails);
+
+        // Trả về thông báo đăng nhập thành công
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         Map<String, String> result = new HashMap<>();
         result.put("message", "Đăng nhập Google thành công!");
+        result.put("token", jwt);
         ObjectMapper mapper = new ObjectMapper();
         response.getWriter().write(mapper.writeValueAsString(result));
     }
